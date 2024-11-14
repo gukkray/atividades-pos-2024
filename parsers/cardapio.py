50d4389cc7903fe1dc75705f3a7ee6cdfa95cf67
Rayssa <raymanoella8812@gmail.com>
2024-11-14 09:13:37 -0300

    tarefa do slide 08 da aula 10

---
+++ parsers/cardapio.py
@@ -0,0 +1,49 @@
+from xml.dom.minidom import parse
+import os
+
+def exibir_menu(pratos):
+    print("Cardápio:")
+    for i, prato in enumerate(pratos, start=1):
+        nome = prato.getElementsByTagName("nome")[0].firstChild.nodeValue
+        print(f"{i} - {nome}")
+
+def exibir_detalhes_prato(pratos, escolha):
+    try:
+        prato = pratos[escolha - 1]
+        nome = prato.getElementsByTagName("nome")[0].firstChild.nodeValue
+        descricao = prato.getElementsByTagName("descricao")[0].firstChild.nodeValue
+        ingredientes = [ing.firstChild.nodeValue for ing in prato.getElementsByTagName("ingredientes")[0].getElementsByTagName("ingrediente")]
+        preco = prato.getElementsByTagName("preco")[0].firstChild.nodeValue
+        calorias = prato.getElementsByTagName("calorias")[0].firstChild.nodeValue
+        tempo_preparo = prato.getElementsByTagName("tempodePreparo")[0].firstChild.nodeValue
+
+        print("\nNome:", nome)
+        print("Descrição:", descricao)
+        print("Ingredientes:")
+        for ingrediente in ingredientes:
+            print("    " + ingrediente)
+        print("Preço:", preco)
+        print("Calorias:", calorias)
+        print("Tempo de preparo:", tempo_preparo)
+    except IndexError:
+        print("Opção inválida. Tente novamente.")
+
+def main():
+    # Define o caminho relativo para o arquivo XML
+    caminho_arquivo_xml = os.path.join("..", "xml", "cardapio.xml")
+    
+    # Carrega o arquivo XML usando o caminho relativo
+    dom = parse(caminho_arquivo_xml)
+    cardapio = dom.documentElement
+    pratos = cardapio.getElementsByTagName("prato")
+    
+    exibir_menu(pratos)
+    
+    try:
+        escolha = int(input("\nDigite o número do prato para saber mais: "))
+        exibir_detalhes_prato(pratos, escolha)
+    except ValueError:
+        print("Entrada inválida. Por favor, insira um número.")
+
+if __name__ == "__main__":
+    main()
